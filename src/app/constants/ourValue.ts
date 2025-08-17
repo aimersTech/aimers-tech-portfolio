eb54eb8a72e8f9765940bab2da913de14f013664
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-17 23:29:42 +0600

    feat: add our value section on about us details page

---
+++ src/app/constants/ourValue.ts
@@ -0,0 +1,31 @@
+export interface IOurValue {
+    title: string,
+    description: string,
+}
+
+export const OurValue: IOurValue[] = [
+    {
+        title: "Innovation-Driven ",
+        description: "We constantly explore new technologies and creative solutions to stay ahead in a rapidly evolving digital world.",
+    },
+    {
+        title: "Integrity & Transparency",
+        description: "Applying machine learning to real-world problems with custom AI solutions and automation",
+    },
+    {
+        title: "Quality & Excellence",
+        description: "We’re committed to delivering work that is not only functional and beautiful but also reliable and impactful.",
+    },
+    {
+        title: "Empathy & Research-First Approach",
+        description: "We dive deep into understanding problems before solving them—because insight leads to innovation.",
+    },
+    {
+        title: "Sustainability & Responsibility",
+        description: "We aim to build technology that not only serves business needs but also supports a better, more sustainable future.",
+    },
+    {
+        title: "Reliability",
+        description: "We deliver consistent, dependable solutions that our clients can trust. From meeting deadlines to maintaining high performance, we ensure every product and service we provide is built to last.",
+    },
+]
