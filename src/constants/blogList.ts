f37cebce16ad78f0ad4f32ef815df0b2e4fb5f28
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-08-25 20:52:38 +0600

    feat: add blog list section

---
+++ src/constants/blogList.ts
@@ -0,0 +1,65 @@
+export interface IBlogs {
+    id: number
+    title: string
+    description: string
+    image: string
+    date: string
+    readTime: string
+}
+
+export const Blogs: IBlogs[] = [
+    {
+    id: 1,
+    title: "AI Won’t Take Your Job – But It Will Change How You Do It",
+    description:
+      "AI won’t replace you, but it will redefine the way you work—automating the routine so you can focus on creativity and strategy.",
+    image: "/blog1.png",
+    date: "July 30, 2025",
+    readTime: "12 min read",
+  },
+  {
+    id: 2,
+    title: "How UX-Driven Design Impacts SaaS Product Success",
+    description:
+      "In the fast-paced world of SaaS, having a powerful product isn’t enough—users expect seamless, intuitive, and delightful experiences.",
+    image: "/blog2.png",
+    date: "July 31, 2025",
+    readTime: "9 min read",
+  },
+  {
+    id: 3,
+    title: "25 LinkedIn Stats To Know For In 2025",
+    description:
+      "In this article, you’ll find 20+ essential LinkedIn statistics that reveal the platform’s reach, effectiveness, and continued growth.",
+    image: "/blog3.png",
+    date: "July 30, 2025",
+    readTime: "15 min read",
+  },
+  {
+    id: 4,
+    title: "AI helps you work fast, smart, and do more tasks.",
+    description:
+      "Artificial Intelligence (AI) is no longer a futuristic concept—it’s now a vital part of how we work.",
+    image: "/blog4.png",
+    date: "July 31, 2025",
+    readTime: "6 min read",
+  },
+  {
+    id: 5,
+    title: "AI Won’t Take Your Job – But It Will Change How You Do It",
+    description:
+      "AI won’t replace you, but it will redefine the way you work—automating the routine so you can focus on creativity and strategy.",
+    image: "/blog1.png",
+    date: "July 30, 2025",
+    readTime: "12 min read",
+  },
+  {
+    id: 6,
+    title: "AI helps you work fast, smart, and do more tasks.",
+    description:
+      "Artificial Intelligence (AI) is no longer a futuristic concept—it’s now a vital part of how we work.",
+    image: "/blog4.png",
+    date: "July 31, 2025",
+    readTime: "6 min read",
+  },
+]
